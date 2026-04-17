bade510c77ddc454ce7b1c21e80dbcdde341e899
cassisrz <cassisrzb@gmail.com>
2026-04-17 20:03:39 +0100

    Refactor GestorCubos to manage Cubo instances
    Refactor GestorCubos class to manage Cubo instances with methods for initialization, sorting, and checking dimensions.

--- GestorCubos.java
+++ GestorCubos.java
@@ -1,47 +1,35 @@
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class GestorCubos {
 
-    public static void main(final String[] args) {
+    private ArrayList<Cubo> listaCubos;
 
-        /** ArrayList que almacenará la información de los cubos */
-        ArrayList<Cubo> cubos = new ArrayList<>();
+    public GestorCubos() {
+        listaCubos = new ArrayList<>();
+    }
 
-        /** Bucle para pedir la info de todos los cubos */
-        for (int i = 0; i < 3; i++) {
-            Cubo cubo = new Cubo(0);
-            cubo.solicitarLado();
-            cubos.add(cubo);
-        }
-
-        /** String resultado para mostrar los resultados */
-        String resultado = "Resultados:\n\n";
-
-        for (int i = 0; i < cubos.size(); i++) {
-            Cubo c = cubos.get(i);
-
-            resultado += "Cubo " + (i + 1) +
-                    "\nLado: " + c.getLado() +
-                    "\nÁrea base: " + c.calcularAreaBase() +
-                    "\nVolumen: " + c.calcularVolumen() +
-                    "\n----------------------\n";
-        }
-
-        JOptionPane.showMessageDialog(null, resultado);
-
-        /** Calcula el cubo con mayor volumen */
-        Cubo mayor = cubos.get(0);
-
-        for (Cubo c : cubos) {
-            if (c.calcularVolumen() > mayor.calcularVolumen()) {
-                mayor = c;
+    public void inicializarCubos() {
+        listaCubos.add(new Cubo(3));
+        listaCubos.add(new Cubo(5));
+        listaCubos.add(new Cubo(9));
+    }
+    
+    public void ordenarCubos() {
+        Collections.sort(listaCubos, new Comparator<Cubo>() {
+            @Override
+            public int compare(Cubo c1, Cubo c2) {
+                return Float.compare(c1.calcularVolumen(), c2.calcularVolumen());
             }
-        }
+        });
+    }
 
-        JOptionPane.showMessageDialog(null,
-                "Cubo con mayor volumen:\n\n" +
-                        "Lado: " + mayor.getLado() +
-                        "\nVolumen: " + mayor.calcularVolumen());
+    public boolean cabeDentro(Cubo c1, Cubo c2) {
+        return c1.getLado() <= c2.getLado();
+    }
+
+    public ArrayList<Cubo> getListaCubos() {
+        return listaCubos;
     }
 }
