6d2b0cc7f5fbdcc064554ac3da324ae86330ff75
cassisrz <cassisrzb@gmail.com>
2026-04-17 19:21:39 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,6 @@
 public class Main {
 
 	public static void main(final String[] args) {
-		GestorCubos gestor = new GestorCubos();
 
-		gestor.inicializarCubos();
-		gestor.ordenarCubos();
-		
-		System.out.println("Cubos ordenados por volumen (menor a mayor)");
-		for(Cubo c : gestor.getCubos()) {
-			System.out.println("Lado: "+c.getLado()+"\nVolumen: "+c.calcularVolumen());
-		}
-		
-
-		Cubo c1 = new Cubo(3);
-		Cubo c2 = new Cubo(10);
-		
-		System.out.println("¿El cubo 1 cabe exactamente dentro del 2? " + gestor.cabeDentro(c1, c2));
 	}
 }
