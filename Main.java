73972ba22542c6f5cf045b0f65013c700fb5ad7d
cassisrz <cassisrzb@gmail.com>
2026-04-17 19:49:55 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,41 @@
+/**
+ * Programa que incrementa un valor hasta que el resultado
+ * sea igual o mayor a 100
+ */
+
 public class Main {
-
+	/**
+	 * Método principal del programa
+	 * 
+	 * Realiza un bucle que incrementa un valor de total
+	 * en cada iteración hasta llegar a 100 o más.
+	 * 
+	 * @param args
+	 */
 	public static void main(final String[] args) {
-		GestorCubos gestor = new GestorCubos();
-
-		gestor.inicializarCubos();
-		gestor.ordenarCubos();
-
-		System.out.println("Cubos ordenados por volumen:");
-
-		for(Cubo c : gestor.getCubos()) {
-			System.out.println("Lado: "+c.getLado()+"\nVolumen: "+c.calcularVolumen());
-		}
-
-		Cubo c1 = new Cubo(3);
-		Cubo c2 = new Cubo(10);
-
-		System.out.println("¿El cubo 1 cabe dentro del 2? "+gestor.cabeDentro(c1, c2));
+		/** Resultado final del proceso */
+		@SuppressWarnings("unused")
+		int resultado = 0;
+		/** Número base usado para calcular incrementos */
+		int numBase = 15;
+		/** Incremento calculado en cada iteración */
+		int incremento = 20;
+		/** Variable que acumula el valor total */
+		int total = 0;
+		do {
+			incremento = 10 + numBase;
+			total = total + incremento;
+			total++;
+			if (total < 10) {
+				resultado = 15;	
+				total = total - 1;
+			} else {
+				resultado = 10;
+				total = total - 1;
+			}
+			int copiaTotal;
+			copiaTotal = total;
+			resultado = copiaTotal;
+		} while (total < 100);
 	}
 }
